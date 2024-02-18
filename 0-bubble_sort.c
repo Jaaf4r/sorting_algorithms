6f833fb2ed272b3fc4d7b77f39160fbdfeef0321
Jaaf4r <jaafar.bouhnine@gmail.com>
2024-02-18 12:06:15 +0100

    sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,11 +7,11 @@
 */
 void swap_ints(int *a, int *b)
 {
-    int tmp;
+	int tmp;
 
-    tmp = *a;
-    *a = *b;
-    *b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -19,29 +19,29 @@ void swap_ints(int *a, int *b)
  *               using the Bubble sort algorithm.
  * @array: An array of integers to sort.
  * @size: The size of the array.
- * 
+ *
  * Description: Prints the array after each swap.
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i = 0, len = size - 1;
-    int flag = 1;
+	size_t i = 0, len = size - 1;
+	int flag = 1;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    while (flag == 1)
-    {
-        flag = 0;
-        for (i = 0; i < len; i++)
-        {
-            if (array[i] > array[i + 1])
-            {
-                swap_ints(array + i, array + i + 1);
-                print_array(array, size);
-                flag = 1;
-            }
-        }
-        len--;
-    }
+	while (flag == 1)
+	{
+		flag = 0;
+		for (i = 0; i < len; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				flag = 1;
+			}
+		}
+		len--;
+	}
 }
